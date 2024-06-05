0c59bb08bd69849d2ac8e850fe3dfa0a5536bb42
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2024-06-05 15:37:17 +0530

    feat: the vault page requires authentication to view content

--- src/components/vaultpage/VaultPage.tsx
+++ src/components/vaultpage/VaultPage.tsx
@@ -3,11 +3,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { serverAction } from "~/server/actions";
 import { Button } from "../ui/button";
+import { useAccount } from "wagmi";
+
+export const dynamic = "force-dynamic";
 
 export default function VaultPage() {
   const { mutate, isPending } = useMutation({
     mutationFn: serverAction,
   });
+  const { isConnected } = useAccount()
+
+  if (!isConnected) return <div className="text-center">please login above</div>;
+
   return (
     <div className="flex items-center justify-center gap-4 py-8">
       Vault Page
